173fe39c0a6eeffac261ee99fa303afe46d05e0c
Sarai Misidjan <Sarai@conduction.nl>
2023-01-26 16:17:29 +0100

    added the developerOverheidGetComponents command

--- Command/DeveloperOverheidGetComponentsCommand.php
+++ Command/DeveloperOverheidGetComponentsCommand.php
@@ -2,40 +2,50 @@
 
 namespace OpenCatalogi\OpenCatalogiBundle\Command;
 
-use OpenCatalogi\OpenCatalogiBundle\Service\FindGithubRepositoryThroughOrganizationService;
+use OpenCatalogi\OpenCatalogiBundle\Service\DeveloperOverheidService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
- * Command to execute the FindGithubRepositoryThroughOrganizationService
+ * Command to execute the DeveloperOverheidService
  */
 class DeveloperOverheidGetComponentsCommand extends Command
 {
+    // the name of the command (the part after "bin/console")
     protected static $defaultName = 'opencatalogi:developeroverheid:components';
-    private FindGithubRepositoryThroughOrganizationService  $findGithubRepositoryThroughOrganizationService;
+    private DeveloperOverheidService  $developerOverheidService;
 
 
-    public function __construct(FindGithubRepositoryThroughOrganizationService $findGithubRepositoryThroughOrganizationService)
+    public function __construct(DeveloperOverheidService $developerOverheidService)
     {
-        $this->findGithubRepositoryThroughOrganizationService = $findGithubRepositoryThroughOrganizationService;
+        $this->developerOverheidService = $developerOverheidService;
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this
-            ->setDescription('This command triggers OpenCatalogi FindGithubRepositoryThroughOrganizationService')
-            ->setHelp('This command allows you to update a organizations with found opencatalogi.yml info');
+            ->setDescription('This command triggers OpenCatalogi DeveloperOverheidService')
+            ->setHelp('This command allows you to get all components or one component from developer.overheid.nl/apis')
+            ->addOption('component', 'c', InputOption::VALUE_OPTIONAL, 'Get a singe component by id or name');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $this->findGithubRepositoryThroughOrganizationService->setStyle($io);
+        $this->developerOverheidService->setStyle($io);
 
-        $this->findGithubRepositoryThroughOrganizationService->findGithubRepositoryThroughOrganizationHandler();
+        // Handle the command optiosn
+        $componentId = $input->getOption('component', false);
+
+        if(!$componentId){
+            $this->developerOverheidService->getComponents();
+        } else{
+            $this->developerOverheidService->getComponent($componentId);
+        }
 
         return 0;
     }
