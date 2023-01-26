55a5626a435d6807377e24a92a498b6dbb3b15fc
Sarai Misidjan <Sarai@conduction.nl>
2023-01-26 12:03:06 +0100

    fix import developerOverheidService

--- Command/DeveloperOverheidGetRepositoriesCommand.php
+++ Command/DeveloperOverheidGetRepositoriesCommand.php
@@ -2,7 +2,7 @@
 
 namespace OpenCatalogi\OpenCatalogiBundle\Command;
 
-use OpenCatalogi\OpenCatalogiBundle\Service\FindGithubRepositoryThroughOrganizationService;
+use OpenCatalogi\OpenCatalogiBundle\Service\DeveloperOverheidService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
