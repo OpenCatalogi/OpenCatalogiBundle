c07fa3f75385e6d2443203a80dc0cc5680034b63
Sarai Misidjan <Sarai@conduction.nl>
2023-01-26 17:04:48 +0100

    componentenCatalogus applications and components commands

--- Command/ComponentenCatalogusGetApplicationsCommand.php
+++ Command/ComponentenCatalogusGetApplicationsCommand.php
@@ -2,9 +2,10 @@
 
 namespace OpenCatalogi\OpenCatalogiBundle\Command;
 
-use OpenCatalogi\OpenCatalogiBundle\Service\FindGithubRepositoryThroughOrganizationService;
+use OpenCatalogi\OpenCatalogiBundle\Service\ComponentenCatalogusService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -14,28 +15,36 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class ComponentenCatalogusGetApplicationsCommand extends Command
 {
     protected static $defaultName = 'opencatalogi:componentencatalogus:applications';
-    private FindGithubRepositoryThroughOrganizationService  $findGithubRepositoryThroughOrganizationService;
+    private ComponentenCatalogusService  $componentenCatalogusService;
 
 
-    public function __construct(FindGithubRepositoryThroughOrganizationService $findGithubRepositoryThroughOrganizationService)
+    public function __construct(ComponentenCatalogusService $componentenCatalogusService)
     {
-        $this->findGithubRepositoryThroughOrganizationService = $findGithubRepositoryThroughOrganizationService;
+        $this->componentenCatalogusService = $componentenCatalogusService;
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this
-            ->setDescription('This command triggers OpenCatalogi FindGithubRepositoryThroughOrganizationService')
-            ->setHelp('This command allows you to update a organizations with found opencatalogi.yml info');
+            ->setDescription('This command triggers OpenCatalogi ComponentenCatalogusService')
+            ->setHelp('This command allows you to get all applications or one application from componentencatalogus.commonground.nl/api/products')
+            ->addOption('application', 'a', InputOption::VALUE_OPTIONAL, 'Get a singe application by id or name');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $this->findGithubRepositoryThroughOrganizationService->setStyle($io);
+        $this->componentenCatalogusService->setStyle($io);
 
-        $this->findGithubRepositoryThroughOrganizationService->findGithubRepositoryThroughOrganizationHandler();
+        // Handle the command optiosn
+        $applicationId = $input->getOption('application', false);
+
+        if(!$applicationId){
+            $this->componentenCatalogusService->getApplications();
+        } else{
+            $this->componentenCatalogusService->getApplication($applicationId);
+        }
 
         return 0;
     }
--- Command/ComponentenCatalogusGetComponentsCommand.php
+++ Command/ComponentenCatalogusGetComponentsCommand.php
@@ -2,9 +2,10 @@
 
 namespace OpenCatalogi\OpenCatalogiBundle\Command;
 
-use OpenCatalogi\OpenCatalogiBundle\Service\FindGithubRepositoryThroughOrganizationService;
+use OpenCatalogi\OpenCatalogiBundle\Service\ComponentenCatalogusService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -14,28 +15,36 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class ComponentenCatalogusGetComponentsCommand extends Command
 {
     protected static $defaultName = 'opencatalogi:componentencatalogus:components';
-    private FindGithubRepositoryThroughOrganizationService  $findGithubRepositoryThroughOrganizationService;
+    private ComponentenCatalogusService  $componentenCatalogusService;
 
 
-    public function __construct(FindGithubRepositoryThroughOrganizationService $findGithubRepositoryThroughOrganizationService)
+    public function __construct(ComponentenCatalogusService $componentenCatalogusService)
     {
-        $this->findGithubRepositoryThroughOrganizationService = $findGithubRepositoryThroughOrganizationService;
+        $this->componentenCatalogusService = $componentenCatalogusService;
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this
-            ->setDescription('This command triggers OpenCatalogi FindGithubRepositoryThroughOrganizationService')
-            ->setHelp('This command allows you to update a organizations with found opencatalogi.yml info');
+            ->setDescription('This command triggers OpenCatalogi ComponentenCatalogusService')
+            ->setHelp('This command allows you to get all components or one component from componentencatalogus.commonground.nl/api/components')
+            ->addOption('component', 'c', InputOption::VALUE_OPTIONAL, 'Get a singe component by id or name');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $this->findGithubRepositoryThroughOrganizationService->setStyle($io);
+        $this->componentenCatalogusService->setStyle($io);
 
-        $this->findGithubRepositoryThroughOrganizationService->findGithubRepositoryThroughOrganizationHandler();
+        // Handle the command optiosn
+        $componentId = $input->getOption('component', false);
+
+        if(!$componentId){
+            $this->componentenCatalogusService->getComponents();
+        } else{
+            $this->componentenCatalogusService->getComponent($componentId);
+        }
 
         return 0;
     }
