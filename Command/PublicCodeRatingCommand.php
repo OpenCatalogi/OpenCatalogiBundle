5a0c975574ec526f75c4b8b50c95af27a2653bd6
Wilco Louwerse <wilco@conduction.nl>
2023-01-30 14:25:43 +0100

    fix missing use

--- Command/PublicCodeRatingCommand.php
+++ Command/PublicCodeRatingCommand.php
@@ -5,6 +5,7 @@ namespace OpenCatalogi\OpenCatalogiBundle\Command;
 use OpenCatalogi\OpenCatalogiBundle\Service\RatingService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
