e7071377c3b6fbdec6b47ea012c0b754532359dc
Wilco Louwerse <wilco@conduction.nl>
2023-03-13 16:03:20 +0100

    Let's try to move the search endpoint to installation.json

--- Service/InstallationService.php
+++ Service/InstallationService.php
@@ -1,7 +1,5 @@
 <?php
 
-// src/Service/LarpingService.php
-
 namespace OpenCatalogi\OpenCatalogiBundle\Service;
 
 use App\Entity\Cronjob;
@@ -123,97 +121,45 @@ class InstallationService implements InstallerInterface
         }
     }
 
-//    public function createSources()
-//    {
-//        $sourceRepository = $this->entityManager->getRepository('App:Gateway');
-//
-//        // componentencatalogus
-//        $componentenCatalogusSource = $sourceRepository->findOneBy(['name' => 'componentencatalogus']) ?? new Source();
-//        $componentenCatalogusSource->setName('componentencatalogus');
-//        $componentenCatalogusSource->setAuth('none');
-//        $componentenCatalogusSource->setLocation('https://componentencatalogus.commonground.nl/api');
-//        $componentenCatalogusSource->setIsEnabled(true);
-//        $this->entityManager->persist($componentenCatalogusSource);
-//        isset($this->io) && $this->io->writeln('Gateway: '.$componentenCatalogusSource->getName().' created');
-//
-//        // developer.overheid
-//        $developerOverheid = $sourceRepository->findOneBy(['name' => 'developerOverheid']) ?? new Source();
-//        $developerOverheid->setName('developerOverheid');
-//        $developerOverheid->setAuth('none');
-//        $developerOverheid->setLocation('https://developer.overheid.nl/api');
-//        $developerOverheid->setIsEnabled(true);
-//        $this->entityManager->persist($developerOverheid);
-//        isset($this->io) && $this->io->writeln('Gateway: '.$developerOverheid->getName().' created');
-//
-//        // GitHub API
-//        $gitHubAPI = $sourceRepository->findOneBy(['name' => 'GitHub API']) ?? new Source();
-//        $gitHubAPI->setName('GitHub API');
-//        $gitHubAPI->setAuth('apikey');
-//        $gitHubAPI->setHeaders(['Accept' => 'application/vnd.github+json']);
-//        $gitHubAPI->setAuthorizationHeader('Authorization');
-//        $gitHubAPI->setAuthorizationPassthroughMethod('header');
-//        $gitHubAPI->setLocation('https://api.github.com');
-//        $gitHubAPI->setIsEnabled(true);
-//        $this->entityManager->persist($gitHubAPI);
-//
-//        // TODO: move this to installation.json!!!
-////        $dashboardCard = new DashboardCard($gitHubAPI);
-////        $this->entityManager->persist($dashboardCard);
-//        isset($this->io) && $this->io->writeln('Gateway: '.$gitHubAPI->getName().' created');
-//
-//        // GitHub usercontent
-//        $gitHubUserContentSource = $sourceRepository->findOneBy(['name' => 'GitHub usercontent']) ?? new Source();
-//        $gitHubUserContentSource->setName('GitHub usercontent');
-//        $gitHubUserContentSource->setAuth('none');
-//        $gitHubUserContentSource->setLocation('https://raw.githubusercontent.com');
-//        $gitHubUserContentSource->setIsEnabled(true);
-//        $this->entityManager->persist($gitHubUserContentSource);
-//        isset($this->io) && $this->io->writeln('Gateway: '.$gitHubUserContentSource->getName().' created');
-//
-//        // flush the sources before adding actions via the addActions function
-//        // we need the id of the sources
-//        $this->entityManager->flush();
-//    }
-
     public function checkDataConsistency()
     {
         // set all entity maxDepth to 5
         $this->setEntityMaxDepth();
 
-        // Doesn't work so let's let search endpoint return all
-        $schemasToAddToSearchEndpoint = [
-            'https://opencatalogi.nl/oc.application.schema.json',
-            'https://opencatalogi.nl/oc.organisation.schema.json',
-            'https://opencatalogi.nl/oc.component.schema.json',
-        ];
-
-        $schemas = [];
-        foreach ($schemasToAddToSearchEndpoint as $schema) {
-            $foundSchema = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $schema]);
-            if ($foundSchema instanceof Entity) {
-                $schemas[] = $foundSchema;
-            } else {
-                isset($this->io) && $this->io->writeln('Schema: '.$schema.' could not be found. Installation failed');
-
-                throw new Exception('Schema: '.$schema.' could not be found. Installation failed');
-            }
-        }
-
-        // Lets see if there is a generic search endpoint
-        if (!$searchEndpoint = $this->entityManager->getRepository('App:Endpoint')->findOneBy(['pathRegex' => '^(search)$'])) {
-            $searchEndpoint = new Endpoint();
-            $searchEndpoint->setName('Search');
-            $searchEndpoint->setDescription('Generic Search Endpoint');
-            $searchEndpoint->setPath(['search']);
-            $searchEndpoint->setPathRegex('^(search)$');
-            $searchEndpoint->setMethod('GET');
-            $searchEndpoint->setMethods(['GET']);
-            $searchEndpoint->setOperationType('collection');
-            foreach ($schemas as $schema) {
-                $searchEndpoint->addEntity($schema);
-            }
-            $this->entityManager->persist($searchEndpoint);
-        }
+//        // Doesn't work so let's let search endpoint return all
+//        $schemasToAddToSearchEndpoint = [
+//            'https://opencatalogi.nl/oc.application.schema.json',
+//            'https://opencatalogi.nl/oc.organisation.schema.json',
+//            'https://opencatalogi.nl/oc.component.schema.json',
+//        ];
+//
+//        $schemas = [];
+//        foreach ($schemasToAddToSearchEndpoint as $schema) {
+//            $foundSchema = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $schema]);
+//            if ($foundSchema instanceof Entity) {
+//                $schemas[] = $foundSchema;
+//            } else {
+//                isset($this->io) && $this->io->writeln('Schema: '.$schema.' could not be found. Installation failed');
+//
+//                throw new Exception('Schema: '.$schema.' could not be found. Installation failed');
+//            }
+//        }
+//
+//        // Lets see if there is a generic search endpoint
+//        if (!$searchEndpoint = $this->entityManager->getRepository('App:Endpoint')->findOneBy(['pathRegex' => '^(search)$'])) {
+//            $searchEndpoint = new Endpoint();
+//            $searchEndpoint->setName('Search');
+//            $searchEndpoint->setDescription('Generic Search Endpoint');
+//            $searchEndpoint->setPath(['search']);
+//            $searchEndpoint->setPathRegex('^(search)$');
+//            $searchEndpoint->setMethod('GET');
+//            $searchEndpoint->setMethods(['GET']);
+//            $searchEndpoint->setOperationType('collection');
+//            foreach ($schemas as $schema) {
+//                $searchEndpoint->addEntity($schema);
+//            }
+//            $this->entityManager->persist($searchEndpoint);
+//        }
 
         if (!$githubEventEndpoint = $this->entityManager->getRepository('App:Endpoint')->findOneBy(['pathRegex' => '^(github_events)$'])) {
             $githubEventEndpoint = new Endpoint();
@@ -232,16 +178,6 @@ class InstallationService implements InstallerInterface
 
         // create cronjobs
         $this->createCronjobs();
-
-//        // create sources
-//        $this->createSources();
-
-        // Now we kan do a first federation
-        isset($this->io) && $this->catalogiService->setStyle($this->io);
-        //$this->catalogiService->readCatalogi($opencatalogi);
-
-        /*@todo register this catalogi to the federation*/
-        // This requers a post to a pre set webhook
 
         $this->entityManager->flush();
     }
