d267ff13e61f014491ce4bfeb61dd2ea862df1e1
Sarai Misidjan <Sarai.misidjan@hotmail.com>
2023-11-10 09:35:02 +0100

    return component object

--- src/Service/FindGithubRepositoryThroughOrganizationService.php
+++ src/Service/FindGithubRepositoryThroughOrganizationService.php
@@ -255,7 +255,7 @@ class FindGithubRepositoryThroughOrganizationService
         $this->entityManager->persist($sync->getObject());
         $this->entityManager->flush();
 
-        return $repositoryObject;
+        return $sync->getObject();
 
     }//end getOrganisationRepo()
 
