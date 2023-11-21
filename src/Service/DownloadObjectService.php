3815d29829619a1c31968bf5a63e691c0e58b0cd
Robert Zondervan <robert@conduction.nl>
2023-11-21 13:39:36 +0100

    Add documentation, make existing documentation follow standards

--- src/Service/DownloadObjectService.php
+++ src/Service/DownloadObjectService.php
@@ -14,6 +14,9 @@ class DownloadObjectService
     private EntityManagerInterface $entityManager;
 
 
+    /**
+     * @param EntityManagerInterface $entityManager The entity manager.
+     */
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
@@ -21,6 +24,13 @@ class DownloadObjectService
     }//end __construct()
 
 
+    /**
+     * Returns the content type for well known file formats.
+     *
+     * @param string $extension The extension of the file.
+     *
+     * @return string The resulting content type, the extension if the content type is not known.
+     */
     public function getContentType(string $extension): string
     {
         switch ($extension) {
@@ -49,6 +59,15 @@ class DownloadObjectService
     }//end getContentType()
 
 
+    /**
+     * Perform a call on the server to retrieve the size of the download.
+     *
+     * NOTE: not all servers return a truthful answer, some might return just -1.
+     *
+     * @param string $url The url to perform the call on.
+     *
+     * @return int The size of the download.
+     */
     public function getFileSize(string $url): int
     {
         $ch = curl_init();
@@ -57,20 +76,27 @@ class DownloadObjectService
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
+        // Headers are required to be sent by the server.
         curl_setopt($ch, CURLOPT_HEADER, 1);
-        // HEADER REQURIED
+        // Do not download the content body, just fetch metadata.
         curl_setopt($ch, CURLOPT_NOBODY, 1);
-        // NO CONTENT BODY, DO NOT DOWNLOAD ACTUAL FILE
         curl_exec($ch);
 
+        // Parses the response header and returns the size of the download, or -1 on error.
         $CONTENT_LENGTH = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-        // PARSES THE RESPONSE HEADER AND GET FILE SIZE IN BYTES and -1 ON ERROR.
         curl_close($ch);
         return (int) $CONTENT_LENGTH;
 
     }//end getFileSize()
 
 
+    /**
+     * Adds a name, size, content type to the download object if an url is set.
+     *
+     * @param array $data           The data from the
+     * @param array $configuration
+     * @return array
+     */
     public function enrichDownloadObject(array $data, array $configuration): array
     {
         $object = $this->entityManager->find('App:ObjectEntity', $data['object']->getId()->toString());
@@ -78,22 +104,25 @@ class DownloadObjectService
             return $data;
         }
 
+        // Fetch the url from the object.
         $url = $object->getValue('url');
 
+        // Get the filename by reading the last part of the url path.
         $parsedUrl    = \Safe\parse_url($url);
         $explodedPath = explode('/', $parsedUrl['path']);
         $filename     = end($explodedPath);
 
+        // Get the extension by reading the part of the filename behind the last dot.
         $explodedFilename = explode('.', $filename);
         $extension        = end($explodedFilename);
 
+        // Build the renewed download object.
         $objectArray = [
             'url'     => $url,
             'naam'    => $filename,
             'type'    => $this->getContentType($extension),
             'grootte' => $this->getFileSize($url),
         ];
-
         $object->hydrate($objectArray);
 
         return $data;
