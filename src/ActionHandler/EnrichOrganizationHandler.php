3fa4cbcf4086b63fcbdbe85d52e3dfefefb4abc3
Sarai Misidjan <Sarai@conduction.nl>
2024-01-15 15:03:47 +0100

    updated enrich organization service and handler

--- src/ActionHandler/EnrichOrganizationHandler.php
+++ src/ActionHandler/EnrichOrganizationHandler.php
@@ -44,21 +44,85 @@ class EnrichOrganizationHandler implements ActionHandlerInterface
             'description' => 'This handler enriches the organizations',
             'required'    => [
                 'githubSource',
+                'usercontentSource',
+                'repositorySchema',
+                'repositoryMapping',
                 'organizationSchema',
+                'organizationMapping',
+                'componentSchema',
+                'publiccodeMapping',
+                'opencatalogiMapping',
+                'applicationSchema',
             ],
             'properties'  => [
-                'githubSource'       => [
+                'githubSource'        => [
                     'type'        => 'string',
                     'description' => 'The source of the github api.',
                     'example'     => 'https://opencatalogi.nl/source/oc.GitHubAPI.source.json',
                     'reference'   => 'https://opencatalogi.nl/source/oc.GitHubAPI.source.json',
                     'required'    => true,
                 ],
-                'organizationSchema' => [
+                'usercontentSource'   => [
                     'type'        => 'string',
-                    'description' => 'The organisation schema.',
+                    'description' => 'The source of the raw user content.',
+                    'example'     => 'https://opencatalogi.nl/source/oc.GitHubusercontent.source.json',
+                    'reference'   => 'https://opencatalogi.nl/source/oc.GitHubusercontent.source.json',
+                    'required'    => true,
+                ],
+                'repositorySchema'    => [
+                    'type'        => 'string',
+                    'description' => 'The repository schema.',
+                    'example'     => 'https://opencatalogi.nl/oc.repository.schema.json',
+                    'reference'   => 'https://opencatalogi.nl/oc.repository.schema.json',
+                    'required'    => true,
+                ],
+                'repositoryMapping'   => [
+                    'type'        => 'string',
+                    'description' => 'The mapping for github repository to oc repository.',
+                    'example'     => 'https://api.github.com/oc.githubRepository.mapping.json',
+                    'reference'   => 'https://api.github.com/oc.githubRepository.mapping.json',
+                    'required'    => true,
+                ],
+                'organizationSchema'  => [
+                    'type'        => 'string',
+                    'description' => 'The organization schema.',
                     'example'     => 'https://opencatalogi.nl/oc.organisation.schema.json',
                     'reference'   => 'https://opencatalogi.nl/oc.organisation.schema.json',
+                    'required'    => true,
+                ],
+                'organizationMapping' => [
+                    'type'        => 'string',
+                    'description' => 'The mapping for github organization to oc organization.',
+                    'example'     => 'https://api.github.com/oc.githubOrganization.mapping.json',
+                    'reference'   => 'https://api.github.com/oc.githubOrganization.mapping.json',
+                    'required'    => true,
+                ],
+                'componentSchema'     => [
+                    'type'        => 'string',
+                    'description' => 'The component schema.',
+                    'example'     => 'https://opencatalogi.nl/oc.component.schema.json',
+                    'reference'   => 'https://opencatalogi.nl/oc.component.schema.json',
+                    'required'    => true,
+                ],
+                'publiccodeMapping'   => [
+                    'type'        => 'string',
+                    'description' => 'The mapping for publiccode file to oc component.',
+                    'example'     => 'https://api.github.com/oc.githubPubliccodeComponent.mapping.json',
+                    'reference'   => 'https://api.github.com/oc.githubPubliccodeComponent.mapping.json',
+                    'required'    => true,
+                ],
+                'opencatalogiMapping' => [
+                    'type'        => 'string',
+                    'description' => 'The mapping for opencatalogi file to oc organization.',
+                    'example'     => 'https://api.github.com/oc.githubOpenCatalogiYamlToOrg.mapping.json',
+                    'reference'   => 'https://api.github.com/oc.githubOpenCatalogiYamlToOrg.mapping.json',
+                    'required'    => true,
+                ],
+                'applicationSchema'   => [
+                    'type'        => 'string',
+                    'description' => 'The application schema.',
+                    'example'     => 'https://opencatalogi.nl/oc.application.schema.json',
+                    'reference'   => 'https://opencatalogi.nl/oc.application.schema.json',
                     'required'    => true,
                 ],
             ],
@@ -78,15 +142,22 @@ class EnrichOrganizationHandler implements ActionHandlerInterface
     public function run(array $data, array $configuration): array
     {
 
+        try {
+            $this->data['response'] = \Safe\json_decode($data['response']->getContent(), true);
+        } catch (\Exception $exception) {
+            //
+        }
+
         // This comes from the GithubEvent or FormInput action.
         // We hava an organization in the response.
         $organizationId = null;
-        if (key_exists('response', $data) === true
-            && key_exists('_self', $data['response']) === true
-            && key_exists('id', $data['response']['_self']) === true
+        if (key_exists('response', $this->data) === true
+            && key_exists('_self', $this->data['response']) === true
+            && key_exists('id', $this->data['response']['_self']) === true
         ) {
-            $organizationId = $data['response']['_self']['id'];
+            $organizationId = $this->data['response']['_self']['id'];
         }//end if
+
 
         return $this->service->enrichOrganizationHandler($data, $configuration, $organizationId);
 
--- src/Service/EnrichOrganizationService.php
+++ src/Service/EnrichOrganizationService.php
@@ -21,6 +21,12 @@ use Twig\Error\SyntaxError;
 /**
  * Gets an organization from the response of the githubEventAction and formInputAction
  * and enriches the organization.
+ *
+ * @Author Sarai Misidjan <sarai@conduction.nl>
+ *
+ * @license EUPL <https://github.com/ConductionNL/contactcatalogus/blob/master/LICENSE.md>
+ *
+ * @category Service
  */
 class EnrichOrganizationService
 {
@@ -41,9 +47,29 @@ class EnrichOrganizationService
     private GatewayResourceService $resourceService;
 
     /**
+     * @var SynchronizationService
+     */
+    private SynchronizationService $syncService;
+
+    /**
      * @var GithubApiService
      */
     private GithubApiService $githubApiService;
+
+    /**
+     * @var GitlabApiService
+     */
+    private GitlabApiService $gitlabApiService;
+
+    /**
+     * @var PubliccodeService
+     */
+    private PubliccodeService $publiccodeService;
+
+    /**
+     * @var OpenCatalogiService
+     */
+    private OpenCatalogiService $openCatalogiService;
 
     /**
      * @var array
@@ -60,22 +86,170 @@ class EnrichOrganizationService
      * @param EntityManagerInterface $entityManager   The Entity Manager Interface
      * @param LoggerInterface        $pluginLogger    The plugin version of the logger interface
      * @param GatewayResourceService $resourceService The Gateway Resource Service.
+     * @param SynchronizationService $syncService The Synchronization Service
+     * @param PubliccodeService $publiccodeService The publiccode service
+     * @param OpenCatalogiService $openCatalogiService The opencatalogi service
      */
     public function __construct(
         EntityManagerInterface $entityManager,
         LoggerInterface $pluginLogger,
         GatewayResourceService $resourceService,
-        GithubApiService $githubApiService
-    ) {
+        SynchronizationService $syncService,
+        GithubApiService $githubApiService,
+        GitlabApiService $gitlabApiService,
+        PubliccodeService $publiccodeService,
+        OpenCatalogiService $openCatalogiService
+   ) {
         $this->entityManager    = $entityManager;
         $this->pluginLogger     = $pluginLogger;
         $this->resourceService  = $resourceService;
+        $this->syncService = $syncService;
         $this->githubApiService = $githubApiService;
+        $this->gitlabApiService = $gitlabApiService;
+        $this->publiccodeService = $publiccodeService;
+        $this->openCatalogiService = $openCatalogiService;
 
         $this->configuration = [];
         $this->data          = [];
 
     }//end __construct()
+
+
+    /**
+     * This function enriches the repository with a organization and/or component.
+     *
+     * @param ObjectEntity $organization The organization object.
+     * @param array        $opencatalogi opencatalogi file array from the github usercontent/github api call.
+     * @param Source       $source       The github api source.
+     *
+     * @return ObjectEntity The repository object
+     * @throws Exception
+     */
+    public function getConnectedComponents(ObjectEntity $organization, array $opencatalogi, Source $source): ObjectEntity
+    {
+        $repositorySchema   = $this->resourceService->getSchema($this->configuration['repositorySchema'], 'open-catalogi/open-catalogi-bundle');
+        $organizationSchema = $this->resourceService->getSchema($this->configuration['organizationSchema'], 'open-catalogi/open-catalogi-bundle');
+
+        if (key_exists('softwareOwned', $opencatalogi) === true) {
+            $ownedComponents = [];
+            foreach ($opencatalogi['softwareOwned'] as $repositoryUrl) {
+                $repositorySync = $this->syncService->findSyncBySource($source, $repositorySchema, $repositoryUrl);
+
+                // Get the object of the sync if there is one.
+                if ($repositorySync->getObject() !== null) {
+                    $repository = $repositorySync->getObject();
+                }
+
+                // Get the github repository from the given url if the object is null.
+                if ($repositorySync->getObject() === null) {
+                    $this->entityManager->remove($repositorySync);
+                    $this->entityManager->flush();
+                    $this->githubApiService->setConfiguration($this->configuration);
+                    $repository = $this->githubApiService->getGithubRepository($repositoryUrl);
+                }
+
+                // Set the components of the repository to the array.
+                foreach ($repository->getValue('components') as $component) {
+                    $ownedComponents[] = $component;
+                }
+            }
+
+            $organization->hydrate(['owns' => $ownedComponents]);
+            $this->entityManager->persist($organization);
+            $this->entityManager->flush();
+        }//end if
+
+        if (key_exists('softwareSupported', $opencatalogi) === true) {
+            $supportedComponents = [];
+            foreach ($opencatalogi['softwareSupported'] as $supports) {
+                if (key_exists('software', $supports) === false) {
+                    continue;
+                }
+
+                $repositorySync = $this->syncService->findSyncBySource($source, $repositorySchema, $supports['software']);
+
+                // Get the object of the sync if there is one.
+                if ($repositorySync->getObject() !== null) {
+                    $repository = $repositorySync->getObject();
+                }
+
+                // Get the github repository from the given url if the object is null.
+                if ($repositorySync->getObject() === null) {
+                    $this->entityManager->remove($repositorySync);
+                    $this->entityManager->flush();
+                    $this->githubApiService->setConfiguration($this->configuration);
+                    $repository = $this->githubApiService->getGithubRepository($supports['software']);
+                }
+
+                // Set the components of the repository
+                foreach ($repository->getValue('components') as $component) {
+                    $supportedComponents[] = $component;
+                }
+            }//end foreach
+
+            $organization->hydrate(['supports' => $supportedComponents]);
+            $this->entityManager->persist($organization);
+            $this->entityManager->flush();
+        }//end if
+
+        if (key_exists('softwareUsed', $opencatalogi) === true) {
+            $usedComponents = [];
+            foreach ($opencatalogi['softwareUsed'] as $repositoryUrl) {
+                $repositorySync = $this->syncService->findSyncBySource($source, $repositorySchema, $repositoryUrl);
+
+                // Get the object of the sync if there is one.
+                if ($repositorySync->getObject() !== null) {
+                    $repository = $repositorySync->getObject();
+                }
+
+                // Get the github repository from the given url if the object is null.
+                if ($repositorySync->getObject() === null) {
+                    $this->entityManager->remove($repositorySync);
+                    $this->entityManager->flush();
+
+                    $this->githubApiService->setConfiguration($this->configuration);
+                    $repository = $this->githubApiService->getGithubRepository($repositoryUrl);
+                }
+
+                // Set the components of the repository
+                foreach ($repository->getValue('components') as $component) {
+                    $usedComponents[] = $component;
+                }
+            }
+
+            $organization->hydrate(['uses' => $usedComponents]);
+            $this->entityManager->persist($organization);
+            $this->entityManager->flush();
+        }//end if
+
+        if (key_exists('members', $opencatalogi) === true) {
+            $members = [];
+            foreach ($opencatalogi['members'] as $organizationUrl) {
+                $organizationSync = $this->syncService->findSyncBySource($source, $organizationSchema, $organizationUrl);
+
+                if ($organizationSync->getObject() === null) {
+                    // Do we want to get the organization from the repository
+                    // $organizationName = \Safe\parse_url($organizationUrl)['path'];
+                    // $organizationSync = $this->syncService->synchronize($organizationSync, ['github' => $organizationUrl, 'name' => $organizationName]);
+                    //
+                    // $members[] = $organizationSync->getObject();
+                }
+
+                if ($organizationSync->getObject() !== null) {
+                    $members[] = $organizationSync->getObject();
+                }
+            }
+
+            $organization->hydrate(['members' => $members]);
+            $this->entityManager->persist($organization);
+            $this->entityManager->flush();
+        }//end if
+
+        $this->entityManager->flush();
+
+        return $organization;
+
+    }//end getConnectedComponents()
 
 
     /**
@@ -87,7 +261,7 @@ class EnrichOrganizationService
      *
      * @return ObjectEntity
      */
-    public function enrichOrganization(ObjectEntity $organization): ObjectEntity
+    public function enrichGithubOrganization(ObjectEntity $organization): ObjectEntity
     {
         // Get the github api source.
         $source = $this->resourceService->getSource($this->configuration['githubSource'], 'open-catalogi/open-catalogi-bundle');
@@ -110,37 +284,46 @@ class EnrichOrganizationService
             $organizationArray = $this->githubApiService->getUser(trim($githubPath, '/'), $source);
         }
 
-        $opencatalogiUrl = $organization->getValue('opencatalogiRepo');
-        if ($opencatalogiUrl === null) {
+        $opencatalogiRepo = $organization->getValue('opencatalogiRepo');
+
+        // If the opencatalogiRepo is not null get the file and update the organization.
+        if ($opencatalogiRepo !== null) {
+
+            // Get the opencatalogi file from the opencatalogiRepo property.
+            $opencatalogi    = $this->githubApiService->getFileFromRawUserContent($opencatalogiRepo);
+
+            // Get the softwareSupported/softwareOwned/softwareUsed repositories.
+            $organization = $this->getConnectedComponents($organization, $opencatalogi, $source);
+
+            // Enrich the opencatalogi organization with a logo and description.
+            $organization = $this->openCatalogiService->enrichOpencatalogiOrg($organizationArray, $opencatalogi, $organization, $source);
+
+            $this->pluginLogger->info($organization->getValue('name').' succesfully updated the organization with the opencatalogi file.');
+
             return $organization;
         }
 
-        $path = trim(\Safe\parse_url($opencatalogiUrl)['path'], '/');
+        // If the opencatalogiRepo is null update the logo and description with the organization array.
+        if ($opencatalogiRepo === null) {
 
-        // Call the search/code endpoint for publiccode files in this repository.
-        $queryConfig['query'] = ['q' => "filename:opencatalogi extension:yaml extension:yml repo:{$path}"];
-        $opencatalogiFiles    = $this->githubApiService->getFilesFromRepo($source, $queryConfig);
-
-        $opencatalogiNames = [
-            'opencatalogi.yaml',
-            'opencatalogi.yml',
-        ];
-
-        foreach ($opencatalogiFiles as $item) {
-            if (in_array($item['name'], $opencatalogiNames) === true) {
-                $this->githubApiService->setConfiguration($this->configuration);
-                $organization = $this->githubApiService->handleOpencatalogiFile($organizationArray, $source, $item);
-
-                break;
+            // Set the logo and description if null.
+            if ($organization->getValue('logo') === null) {
+                $organization->setValue('logo', $organizationArray['avatar_url']);
             }
-        }
 
-        $this->pluginLogger->debug($organization->getValue('name').' succesfully updated the organization with the opencatalogi file.');
+            if ($organization->getValue('description') === null) {
+                $organization->setValue('description', $organizationArray['description']);
+            }
+
+            $this->entityManager->persist($organization);
+            $this->entityManager->flush();
+
+            $this->pluginLogger->info($organization->getValue('name').' succesfully updated the organization with a logo and/or description.');
+        }
 
         return $organization;
 
     }//end enrichOrganization()
-
 
     /**
      * This function gets all the repositories from the given organization and sets it to the owns of the organization.
@@ -149,21 +332,10 @@ class EnrichOrganizationService
      *
      * @throws GuzzleException|Exception
      *
-     * @return array
+     * @return ObjectEntity
      */
-    public function getOrganization(string $organizationId): array
+    public function getOrganization(string $organizationId): ObjectEntity
     {
-        // Get the response.
-        try {
-            $response = json_decode($this->data['response']->getContent(), true);
-        } catch (Exception $exception) {
-            $this->pluginLogger->error('Cannot get the response.');
-        }
-
-        if (isset($response) === false) {
-            return $this->data;
-        }
-
         // Get the organization object.
         $organization = $this->entityManager->find('App:ObjectEntity', $organizationId);
 
@@ -173,16 +345,16 @@ class EnrichOrganizationService
             && $organization->getValue('github') !== null
         ) {
             // Enrich the organization object.
-            $this->enrichOrganization($organization);
+            return $this->enrichGithubOrganization($organization);
         }//end if
 
         if ($organization instanceof ObjectEntity === false) {
             $this->pluginLogger->error('Could not find given organization');
 
-            return $this->data;
+            return $organization;
         }//end if
 
-        return $this->data;
+        return $organization;
 
     }//end getOrganization()
 
@@ -204,7 +376,9 @@ class EnrichOrganizationService
 
         // If there is an organization in the response.
         if ($organizationId !== null) {
-            $this->getOrganization($organizationId);
+            $organization = $this->getOrganization($organizationId);
+
+            $this->data['response'] = new Response(json_encode($organization->toArray()), 200, ['Content-Type' => 'application/json']);
         }
 
         // If there is no organization we get all the organizations and enrich it.
@@ -219,7 +393,7 @@ class EnrichOrganizationService
                     && $organization->getValue('github') !== null
                 ) {
                     // Enrich the organization object.
-                    $this->enrichOrganization($organization);
+                    $this->enrichGithubOrganization($organization);
                 }//end if
             }
         }
